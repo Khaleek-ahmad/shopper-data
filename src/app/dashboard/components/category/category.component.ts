3ab0e725d7ffa94fb966174b127e781c85ce277c
khaleekahmad <khaleek.ahmad@gmail.com>
2021-08-01 16:46:43 +0530

    test

--- src/app/dashboard/components/category/category.component.ts
+++ src/app/dashboard/components/category/category.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-// https://primefaces.org/primeng/showcase/#/table/lazy
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
